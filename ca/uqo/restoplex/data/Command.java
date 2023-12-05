ee2170e5a6329ba43d3a26c2ee4dc444574e0269
AxelBelin <64922136+AxelBelin@users.noreply.github.com>
2023-12-04 22:02:58 -0500

    Add all implementations of OrderableDescription. WIP : extract all cookables from a Command.

--- ca/uqo/restoplex/data/Command.java
+++ ca/uqo/restoplex/data/Command.java
@@ -1,5 +1,6 @@
 package ca.uqo.restoplex.data;
 import java.util.ArrayList;
+import java.util.List;
 
 public final class Command {
   private enum COMMAND_LINE_STATE {TO_COOK, COOKING, TO_DELIVER, DELIVERED}
@@ -10,5 +11,9 @@ public final class Command {
 
   private Command(Table table) {
     this.table = table;
+  }
+
+  public List<Cookable> extractCookables() {
+    return List.of(); // TODO ICIICIC
   }
 }
